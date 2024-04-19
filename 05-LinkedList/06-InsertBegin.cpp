25384a65a2e83148ce7e2da6dc53bafe999b3964
asrar-codes <asrarmaqbool06@gmail.com>
2024-04-19 16:07:45 +0530

    insert at begin and insert and end in singly linked list

--- 05-LinkedList/06-InsertBegin.cpp
+++ 05-LinkedList/06-InsertBegin.cpp
@@ -14,8 +14,21 @@ struct Node
  }
 };
 
+Node *insertBegin(Node *head, int x)
+{
+ Node *temp = new Node(x);
+
+ temp->next = head;
+ return temp;
+}
 int main()
 {
+ Node *head = new Node(10);
+ head->next = new Node(20);
+ head->next->next = new Node(30);
+
+ Node *newHead = insertBegin(head, 50);
+ cout << newHead->data << " ";
 
  return 0;
 }
--- 05-LinkedList/07-insertEnd.cpp
+++ 05-LinkedList/07-insertEnd.cpp
@@ -14,8 +14,42 @@ struct Node
  }
 };
 
+void insertTail(Node *head, int x)
+{
+ Node *temp = new Node(x);
+ if (head == NULL)
+ {
+  head = temp;
+ }
+ else
+ {
+  Node *curr = head;
+  while (curr->next != NULL)
+  {
+   curr = curr->next;
+  }
+
+  curr->next = temp;
+ }
+}
+
+void printList(Node *head)
+{
+ Node *curr = head;
+ while (curr != NULL)
+ {
+  cout << curr->data << " ";
+  curr = curr->next;
+ }
+}
+
 int main()
 {
+ Node *head = new Node(10);
+ head->next = new Node(20);
+ head->next->next = new Node(30);
 
+ insertTail(head, 50);
+ printList(head);
  return 0;
 }
